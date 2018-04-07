feae00212a9365ad54c1eb6c08368f899953d0a2
DaeData <kkel136@wgu.edu>
2018-04-06 23:42:58 -0500

    Added  Python Scripts for Data Wrangling

---
+++ audit.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 00:44:55 2018
+
+@author: Kris
+"""
+
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+import pprint
+
+OSMFILE = "dallas.osm"
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
+            "Trail", "Parkway", "Commons"]
+
+# UPDATE THIS VARIABLE
+mapping = { "St": "Street",
+            "St.": "Street",
+            "Ave": "Avenue",
+            "Rd": "Road",
+            "Rd.": "Road"
+            }
+
+
+def audit_street_type(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
+
+
+def is_street_name(elem):
+    return (elem.attrib['k'] == "addr:street")
+
+
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+    osm_file.close()
+    return street_types
+
+
+def update_name(name, mapping):
+    if name not in expected:
+        m = street_type_re.search(name)
+        if m:
+            street_type = m.group()
+            if street_type in mapping.keys():
+                name = re.sub(street_type, mapping[street_type], name)
+
+    return name
---
+++ tags.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 00:47:02 2018
+
+@author: Kris
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xml.etree.cElementTree as ET
+import pprint
+import re
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+
+def key_type(element, keys):
+    if element.tag == "tag":
+    
+        key = element.attrib['k']
+        if re.search(lower, key):
+            keys['lower'] +=1
+        elif re.search(lower_colon, key):
+            keys['lower_colon'] +=1
+        elif re.search(problemchars, key):
+            keys['problemchars']+=1
+        else:
+            keys['other']+=1
+        pass
+    return keys    
+
+
+def process_map(filename):
+    keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
+    for _, element in ET.iterparse(filename):
+        keys = key_type(element, keys)
+
+    return keys
+
+
